5906e81a4add202c94af7c90f1dd68daf3aaa686
Freddy Haas <email@frederik-haas.de>
2023-01-02 10:14:37 +0100

    Set-up dummy controller functions

---
+++ controllers/message.js
@@ -0,0 +1,16 @@
+const Message = require('../models/message');
+
+// GET create message
+exports.message_create_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: Message create GET');
+};
+
+// POST create message
+exports.message_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Message create POST');
+};
+
+// View messages
+exports.message_view = (req, res) => {
+    res.send('NOT IMPLEMENTED: Message view');
+};
---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+// GET - Log in
+exports.user_login_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: User log in - GET');
+};
+
+// POST - Log in
+exports.user_login_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: User log in - POST');
+};
+
+// GET - Sign up
+exports.user_signup_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: User sign up - GET');
+};
+
+// POST - Sign up
+exports.user_signup_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: User sign up - POST');
+};
+
+// GET - Become Moderator
+exports.user_become_moderator_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: User become moderator - GET');
+};
+
+// POST - Become Moderator
+exports.user_become_moderator_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: User become moderator - POST');
+};
